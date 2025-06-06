aaffd7cb1c4790e7fe10a2c3e3db2c2cb4bfb6e1
Vishvam -23IT213 <vishvamtank@gmail.com>
2025-06-06 23:03:54 +0530

    Intial commit: Week 1 Work Complete.

---
+++ js/products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: 1,
+    name: "Luxury Shirt",
+    price: 2499,
+    image: "assets/images/shirt.jpg"
+  },
+  {
+    id: 2,
+    name: "Vintage Watch",
+    price: 7599,
+    image: "assets/images/watch.jpg"
+  },
+  {
+    id: 3,
+    name: "Handmade Leather Wallet",
+    price: 1899,
+    image: "assets/images/wallet.jpg"
+  }
+];
